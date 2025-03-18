9362053dbbbe5a9d60fcd570d7b0cf86147aefd6
Luu Truong Toan <luutruongtoanltt@gmail.com>
2025-03-18 20:25:50 +0700

    login page

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,33 +1,39 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const userData = {
-    id: "USR001",
-    name: "Lưu Trường Toán",
-    email: "luutruongtoantt@gmail.com",
-    phone: "0949415422",
-    avatar: "https://example.com/avatar.jpg",
-    dob: "2000-02-25",
-    gender: "male",
-    address: "Cần Thơ, Việt Nam",
-    createdAt: "2025-03-16T12:00:00Z",
-    favoriteMovies: [1126166, 950396, 1149167]
-};
+const userData = [
+    {
+        id: "USR001",
+        name: "Tai Khoan Clone",
+        username: "ltt",
+        password: "123",
+        phone: "0949415422",
+        avatar: "https://example.com/avatar.jpg",
+        dob: "2000-02-25",
+        gender: "male",
+        address: "Cần Thơ, Việt Nam",
+        createdAt: "2025-03-16T12:00:00Z",
+        favoriteMovies: [1126166, 950396, 1149167]
+    },
+    {
+        id: "USR001",
+        name: "Lưu Trường Toán",
+        username: "luutruongtoanltt",
+        password: "123456",
+        phone: "0949415422",
+        avatar: "https://example.com/avatar.jpg",
+        dob: "2000-02-25",
+        gender: "male",
+        address: "Cần Thơ, Việt Nam",
+        createdAt: "2025-03-16T12:00:00Z",
+        favoriteMovies: [1126166, 950396, 1149167]
+    }
+];
 
 export const saveUserProfile = async () => {
     try {
         await AsyncStorage.setItem('userProfile', JSON.stringify(userData));
-        console.log("Lưu profile thành công!");
+        console.log(JSON.stringify(userData));
     } catch (e) {
         console.error("Lỗi khi lưu profile:", e);
-    }
-};
-
-export const getUserProfile = async () => {
-    try {
-        const jsonValue = await AsyncStorage.getItem('userProfile');
-        return jsonValue != null ? JSON.parse(jsonValue) : null;
-    } catch (e) {
-        console.error("Lỗi khi lấy profile:", e);
-        return null;
     }
 };
