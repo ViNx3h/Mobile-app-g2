775e3305f961c0f990cf50b51738607f75ada6f0
ngquocbao99 <baongyen29@gmail.com>
2025-03-25 00:11:58 +0700

    feat: update signup, search

--- src/screens/SearchScreen/SearchScreen.js
+++ src/screens/SearchScreen/SearchScreen.js
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, FlatList, ActivityIndicator, Image } from "react-native";
+import {
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { useRoute } from "@react-navigation/native";
+import { useNavigation } from "@react-navigation/native";
+import Footer from "../../components/Footer/Footer";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import Logo from "../../components/Logo/Logo";
 
 const API_KEY = "fd72fbb755f23fa49c72918520fd2939"; // Thay bằng API key của bạn
 
@@ -9,17 +22,20 @@ export default function SearchScreen() {
   const { query } = route.params; // Nhận query từ SearchBar
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const navigation = useNavigation(); // Hook điều hướng
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const response = await fetch(
-          `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=vi-VN&query=${encodeURIComponent(query)}`
+          `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=vi-VN&query=${encodeURIComponent(
+            query
+          )}`
         );
         const data = await response.json();
         setMovies(data.results || []);
       } catch (error) {
-        console.error("Lỗi khi tìm kiếm phim:", error);
+        console.error("Error when searching for a movie:", error);
       } finally {
         setLoading(false);
       }
@@ -29,12 +45,33 @@ export default function SearchScreen() {
   }, [query]);
 
   return (
-    <View style={{ flex: 1, padding: 16 }}>
-      <Text style={{ fontSize: 20, fontWeight: "bold", marginBottom: 10 }}>
-        Kết quả tìm kiếm: {query}
+    <View className="bg-neutral-800" style={{ flex: 1, padding: 16 }}>
+      {/* Header */}
+      <SafeAreaView
+        style={{ position: "relative", zIndex: 50 }}
+        className="flex-row justify-between items-center mx-4"
+      >
+        <StatusBar style="light" />
+        <Logo />
+        <Text className="text-white text-3xl font-bold">Movies</Text>
+        <SearchBar />
+      </SafeAreaView>
+
+      {/* nội dung */}
+      <Text
+        style={{
+          fontSize: 20,
+          fontWeight: "bold",
+          marginBottom: 10,
+          color: "white",
+        }}
+      >
+        Search results: {query}
       </Text>
       {loading ? (
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
           <ActivityIndicator size="large" color="blue" />
         </View>
       ) : movies.length > 0 ? (
@@ -42,25 +79,42 @@ export default function SearchScreen() {
           data={movies}
           keyExtractor={(item) => item.id.toString()}
           renderItem={({ item }) => (
-            <View style={{ flexDirection: "row", marginBottom: 10 }}>
-              <Image
-                source={{
-                  uri: item.poster_path
-                    ? `https://image.tmdb.org/t/p/w200${item.poster_path}`
-                    : "https://via.placeholder.com/80x120?text=No+Image",
-                }}
-                style={{ width: 80, height: 120, borderRadius: 8 }}
-              />
-              <View style={{ marginLeft: 10, flex: 1 }}>
-                <Text style={{ fontSize: 16, fontWeight: "bold" }}>{item.title}</Text>
-                <Text>{item.release_date || "Không có ngày phát hành"}</Text>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate("MovieDetails", { movieId: item.id })
+              }
+            >
+              <View style={{ flexDirection: "row", marginBottom: 10 }}>
+                <Image
+                  source={{
+                    uri: item.poster_path
+                      ? `https://image.tmdb.org/t/p/w200${item.poster_path}`
+                      : "https://via.placeholder.com/80x120?text=No+Image",
+                  }}
+                  style={{ width: 80, height: 120, borderRadius: 8 }}
+                />
+                <View style={{ marginLeft: 10, flex: 1 }}>
+                  <Text
+                    style={{ fontSize: 16, fontWeight: "bold", color: "white" }}
+                  >
+                    {item.title}
+                  </Text>
+                  <Text
+                    style={{ fontSize: 16, fontWeight: "bold", color: "white" }}
+                  >
+                    {item.release_date || "No release date."}
+                  </Text>
+                </View>
               </View>
-            </View>
+            </TouchableOpacity>
           )}
         />
       ) : (
-        <Text>Không tìm thấy phim nào.</Text>
+        <Text style={{ fontSize: 16, fontWeight: "bold", color: "white" }}>
+          No movies found.
+        </Text>
       )}
+      <Footer />
     </View>
   );
 }
