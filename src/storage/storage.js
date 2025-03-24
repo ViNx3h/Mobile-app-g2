4f5fcf0ab7413707f2c61df5a286d3278e9b29a0
DESKTOP-BJDO86V\lecon <anhlctce172067@fpt.edu.vn>
2025-03-24 13:51:20 +0700

    create profile and favList

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import avata from '../../assets/trending/toan.png'
 
 const userData = [
     {
@@ -7,7 +8,7 @@ const userData = [
         username: "ltt",
         password: "123",
         phone: "081722339",
-        avatar: "https://example.com/avatar.jpg",
+        avatar: avata,
         dob: "2000-02-25",
         gender: "male",
         address: "Cần Thơ, Việt Nam",
@@ -20,19 +21,19 @@ const userData = [
         username: "luutruongtoanltt",
         password: "123456",
         phone: "0949415422",
-        avatar: "https://example.com/avatar.jpg",
+        avatar: avata,
         dob: "2000-02-25",
         gender: "male",
         address: "Cần Thơ, Việt Nam",
         createdAt: "2025-03-16T12:00:00Z",
-        favoriteMovies: [1126166, 950396, 1149167]
+        favoriteMovies: [1125899, 1151470, 822119, 1301650]
     }
 ];
 
 export const saveUserProfile = async () => {
     try {
         await AsyncStorage.setItem('userProfile', JSON.stringify(userData));
-        console.log(JSON.stringify(userData));
+        // console.log(JSON.stringify(userData));
     } catch (e) {
         console.error("Lỗi khi lưu profile:", e);
     }
